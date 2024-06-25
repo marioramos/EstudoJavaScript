a0907cfd87406bf5ba9acc1a49fbb1741c52950f
Mario Ramos <marioramos25@gmail.com>
2024-06-24 23:33:08 -0300

    .

---
+++ 21-Operadores de Comparacao/index.js
@@ -0,0 +1,12 @@
+/*
+Operadores de comparação;
+> Maior que
+>= Maior que ou igual a
+< Maior que
+<= Menor que ou igual a
+== igualdade (valor)*********
+=== igualdade estrita (valor e tipo)
+!= diferente (valor)*******
+!== diferene estrito (valor te tipo)
+*/
+
